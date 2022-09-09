68029a015085770820af2ee9db7fc504ede6d9f2
LordZeuz <chriskwesi12@gmail.com>
2022-09-09 03:21:28 +0000

    betty finally agrees

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-/*
- *main - prints out the function size of
+/**
+ * main - print the results of the size of types
  *
- * Description: using the sizeof() and printf function,
- *we print out the size of the various types in C
- *Return: 0
-*/
+ * Description: using the main function
+ * this programe prints the size of the various types in C
+ * Return: 0
+ */
 
 int main(void)
 {
-  char c;
-  int i;
-  long li;
-  long long lli;
-  float f;
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
-  printf("Size of a char: %ld byte(s)\n", sizeof(c));
-  printf("Size of an int: %ld byte(s)\n", sizeof(i));
-  printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-  printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-  printf("Size of a float: %ld byte(s)\n", sizeof(f));
-  return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
 }
