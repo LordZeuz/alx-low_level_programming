22d152f11e080a7f56bc19156b537a4ad7b9eac9
LordZeuz <chriskwesi12@gmail.com>
2022-09-09 03:07:59 +0000

    learning with betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-/*main - prints out the function size of
-  Description: using the sizeof() and printf function, we print out the size of the various types in C
+/*
+ *main - prints out the function size of
+ *
+ * Description: using the sizeof() and printf function,
+ *we print out the size of the various types in C
+ *Return: 0
 */
 
-int main (void)
+int main(void)
 {
   char c;
   int i;
   long li;
   long long lli;
   float f;
+
   printf("Size of a char: %ld byte(s)\n", sizeof(c));
   printf("Size of an int: %ld byte(s)\n", sizeof(i));
   printf("Size of a long int: %ld byte(s)\n", sizeof(li));
@@ -17,4 +22,3 @@ int main (void)
   printf("Size of a float: %ld byte(s)\n", sizeof(f));
   return (0);
 }
-	
