477548c2028ed406d70dd350e1e94afb20d3dc40
LordZeuz <chriskwesi12@gmail.com>
2022-09-14 00:20:05 +0000

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i = 0;
+
+	while (i <= 9)
+	{
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+	_putchar('\n');
+
+	i++;
+	}
+}
