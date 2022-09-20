9a4db39916ee2b8382690a35cc7b9ec6b8c60e45
LordZeuz <chriskwesi12@gmail.com>
2022-09-20 01:45:14 +0000

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_rev - prints a string in reverse
+ *@s: parameter
+ */
+
+void print_rev(char *s)
+{
+	int count = s;
+
+	while (s[count] != 0)
+	{
+	_putchar('s');
+	count--;
+	}
+}
