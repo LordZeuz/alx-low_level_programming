2eb7abe806727c9f77fecffb491f14f3705194f9
LordZeuz <chriskwesi12@gmail.com>
2022-09-20 02:06:18 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: parameters
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+	putchar(s[j]);
+	}
+}
