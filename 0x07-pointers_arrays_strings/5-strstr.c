4b78f319c1dbc8f97768c9c6b46feafaeb1c5ef5
LordZeuz <chriskwesi12@gmail.com>
2022-09-27 00:45:23 +0000

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,31 +11,26 @@ char  *_strstr(char *haystack, char *needle)
 {
 	int index;
 
-	for (index = 0; haystack[index] >= '\0'; index++)
+	if (*needle == 0)
+	return (haystack);
+
+	while (*haystack)
 	{
-	if (haystack[index] == needle)
-	return (haystack + index);
+	index = 0;
+
+	if (haystack[index] == needle[index])
+	{
+	do {
+	if (needle[index + 1] == '\0')
+	return (haystack);
+
+	index++;
+
+	} while (haystack[index] == needle[index]);
+	}
+
+	haystack++;
 	}
 
 	return ('\0');
-
-
-
-	/**
-	 *while (*haystack != '\0')
-	*{
-	*str1 = haystack;
-	*str2 = needle;
-	*
-	*while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
-	*{
-	* haystack++;
-	* str2++;
-	*}
-	*if (*str2 == '\0')
-	*return (str1);
-	*haystack = str1 + 1;
-	*}
-	*return (0);
-	*/
 }
