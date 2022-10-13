48b0200f04553c7395b0da5e7b8b33aadfd9d6fc
LordZeuz <chriskwesi12@gmail.com>
2022-10-13 00:39:21 +0000

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,17 +9,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int loc, flag = 0;
+	int i;
 
 	if (array == NULL || cmp == NULL)
 	return (-1);
 	if (size <= 0)
 	return (-1);
-	for (loc = 0; loc < size; loc++)
+	for (i = 0; i < size; i++)
 	{
-	flag = cmp(array[loc]);
-	if (flag != 0)
-	return (loc);
+	if (cmp(array[i]) != 0)
+	return (i);
 	}
-	return (-1);
+
 }
