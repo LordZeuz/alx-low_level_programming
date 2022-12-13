13d2a9db00313e6d4b74ce9ea64487c062301e23
LordZeuz <chriskwesi12@gmail.com>
2022-12-13 01:47:05 +0000

    bypass success

--- 0x18-dynamic_libraries/bypass.c
+++ 0x18-dynamic_libraries/bypass.c
@@ -4,7 +4,7 @@
 
 int printf(const char *format, ...)
 {
-	write(1, "9, 8, 10, 24, 75 + 9\n", 22);
+	write(1, "9, 8, 10, 24, 75 9\n", 20);
 	write(1, "Please make me win!\n", 21);
 	exit(EXIT_SUCCESS);
 }
