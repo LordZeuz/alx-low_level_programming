4dd5db5ce6900cd0ca5ced07b9b37c3da608b35b
LordZeuz <chriskwesi12@gmail.com>
2022-12-13 07:32:45 +0000

    sometimes take a rest

--- 0x18-dynamic_libraries/bypass.c
+++ 0x18-dynamic_libraries/bypass.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
+int printf(const char *format, ...)
 {
 	write(1, "9, 8, 10, 24, 75 9\n", 20);
-	write(1, "Please make me win!\n", 21);
+	write(1, "Congratulations, You won the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
